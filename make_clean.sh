50290382ba563a574b5130a2e2726316f5f09442
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-04-01 07:41:55 +1100

    changed some stuff so i can submit lab2

--- make_clean.sh
+++ make_clean.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 clean.sh
-python main.py ~/Homework/Data-Visualisation
-pdftk extra/Lab10.pdf full.pdf cat output fullx.pdf
+python main.py ~/Homework/Data-Visualisation/labsubmission2
+pdftk full.pdf ~/Homework/Data-Visualisation/labsubmission2/Lab-4-1/lab4-1.pdf cat output fullx.pdf
 rm full.pdf
 mv fullx.pdf full.pdf
-pdftk full.pdf cat 1-r2 output fullx.pdf
-rm full.pdf
-mv fullx.pdf full.pdf
+# pdftk full.pdf cat 1-r2 output fullx.pdf
+# rm full.pdf
+# mv fullx.pdf full.pdf
